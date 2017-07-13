7a04fcae397340624abb3fb925420a61bf9a980f
okzapradhana <okzamahendra29@gmail.com>
2017-07-13 14:28:26 +0700

    Update KitchenSinkController.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -171,7 +171,7 @@ public class KitchenSinkController {
     public void handleJoinEvent(JoinEvent event) {
 //         String replyToken = event.getReplyToken();
 //         this.replyText(replyToken, "Joined " + event.getSource());
-        System.out.print("Halo! Terimakasih telah mengundang Ngoding .com ke grup ini");
+        this.replyText(replyToket, "Halo! Terimakasih telah mengundang Nguliah .com ke grup ini");
         
     }
 
